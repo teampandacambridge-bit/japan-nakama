d226de53d0393da06fc5b24e3fb46abcabe69041
lee-oneill-op <lee@r-e-p.com>
2026-07-04 06:48:03 +0100

    Ad plugin

--- themes/woodmart-child/template-parts/ads/ad-sidebar-bottom.php
+++ themes/woodmart-child/template-parts/ads/ad-sidebar-bottom.php
@@ -1,8 +1,1 @@
-<?php
-$ad_destination = 'https://www.zonepress.uk'; // Update this URL at any time
-?>
-<div class="ad-sidebar-bottom" data-theme-uri="<?php echo esc_url(get_stylesheet_directory_uri()); ?>">
-    <a class="ad-link" href="<?php echo esc_url($ad_destination); ?>" target="_blank" rel="sponsored nofollow noopener noreferrer">
-        <img class="ad-img" src="" alt="Advertisement" width="300" height="600" />
-    </a>
-</div>
+<?php echo do_shortcode('[nakama_advert title="vertical-sidebar-bottom"]');
